f3d82758607051c5e71ff27a2f45a3778a2688b3
muzammil-codeverse <muzammilcodeverse@gmail.com>
2024-08-07 20:53:12 +0500

    new little lemon app

---
+++ LittleLemonApp/app/src/main/java/com/example/littlelemonapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.littlelemonapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val layout:LinearLayout = findViewById(R.id.insideLinear)
+        val button: Button = findViewById(R.id.btn)
+        val text = TextView(this)
+        text.text="Order Placed!!!"
+        button.setOnClickListener {
+            layout.addView(text)
+        }
+    }
+}
